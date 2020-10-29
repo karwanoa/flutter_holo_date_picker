56f85e794352e0e59bc7b0e392a5e7895b4122ad
karwanoa <31167079+karwanoa@users.noreply.github.com>
2020-10-29 10:09:55 +0300

    Update strings_ku.dart

--- lib/i18n/strings_ku.dart
+++ lib/i18n/strings_ku.dart
@@ -1,1 +1,52 @@
+part of 'date_picker_i18n.dart';
 
+/// Arabic (KU)
+class _StringsKu extends _StringsI18n {
+  const _StringsKu();
+
+  @override
+  String getCancelText() {
+    return 'پەشیمانبونەوە';
+  }
+
+  @override
+  String getDoneText() {
+    return 'تەواو';
+  }
+
+  @override
+  List<String> getMonths() {
+    return [
+      "کانونی دووەم",
+      "شوبات",
+      "ئازار",
+      "نيسان",
+      "ئایار",
+      "حوزەیران",
+      "تەممووز",
+      "ئاب",
+      "ئەیلول",
+      "تشرینی یەکەم",
+      "تشرینی دووەم",
+      "کانونی یەکەم"
+    ];
+  }
+
+  @override
+  List<String> getWeeksFull() {
+    return [
+      "دوو شەممە",
+      "سێ شەممە",
+      "چوار شەممە",
+      "پێنج شەممە",
+      "هەینی",
+      "شەممە",
+      "یەك شەممە",
+    ];
+  }
+
+  @override
+  List<String> getWeeksShort() {
+    return null;
+  }
+}
